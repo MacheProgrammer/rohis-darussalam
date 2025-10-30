150325ef4ddf5eb084bc0cb10084c4fad615d218
Zudhaaa <zudhaaa1@gmail.com>
2025-10-30 23:03:08 +0700

    skip linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
